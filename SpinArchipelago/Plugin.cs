9be736f668fc1594982e1bfded0214952f34e972
Raoul1808 <raoulthegeek@gmail.com>
2024-10-16 23:42:02 +0200

    Add a bunch of todos for future plans

--- SpinArchipelago/Plugin.cs
+++ SpinArchipelago/Plugin.cs
@@ -9,6 +9,22 @@ namespace SpinArchipelago
     [BepInDependency("srxd.raoul1808.spincore")]
     public class Plugin : BaseUnityPlugin
     {
+        // TODO: Fix clearing counted when failing
+        // TODO: Add goal
+        // TODO: Configure goal
+        // TODO: Add clear conditions
+        // TODO: Add traps
+        // TODO: Trap ideas:
+        // TODO: Chroma Traps (all rainbow, all grayscale, random palette changes)
+        // TODO: Track Speed changes (SONIC, training bandwidth, nauseating slowdown and speedups)
+        // TODO: Distracting camera transforms
+        // TODO: Spoon
+        // TODO: There is no spoon :(
+        // TODO: Changing playback rate (INVALIDATE SCORE!!!!)
+        // TODO: Add rank checkign to force combo (unrelated but todoing anyway)
+        // TODO: 
+        // TODO: 
+        // TODO: 
         private const string Guid = "srxd.raoul1808.spinarchipelago";
         private const string Name = "Spin Archipelago";
         private const string Version = "0.1.0";
