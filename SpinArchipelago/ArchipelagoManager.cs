449cd2bc5702e1698e4e8eb9bac01de194f48602
Raoul1808 <raoulthegeek@gmail.com>
2024-10-16 16:08:15 +0200

    Begin archipelago integration

--- SpinArchipelago/ArchipelagoManager.cs
+++ SpinArchipelago/ArchipelagoManager.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Helpers;
 using SpinCore.UI;
 
 namespace SpinArchipelago
 {
     internal static class ArchipelagoManager
     {
+        private const string DEFAULT_ARCHIPELAGO_URL = "archipelago.gg:38281";
+
         private static string _playerName;
         private static string _serverAddress;
         private static string _password;
+
+        private static ArchipelagoSession _session;
+
+        private static List<int> _unlockedSongs = new List<int>();
 
         public static void InitUI()
         {
@@ -40,11 +51,13 @@ namespace SpinArchipelago
                         "Label",
                         "SpinArchipelago_ServerAddress"
                     );
-                    UIHelper.CreateInputField(
+                    var field = UIHelper.CreateInputField(
                         g.Transform,
                         "Input Field",
                         (_, newValue) => _serverAddress = newValue
                     );
+                    field.InputField.tmpInputField.text = DEFAULT_ARCHIPELAGO_URL;
+                    _serverAddress = DEFAULT_ARCHIPELAGO_URL;
                 }
                 {
                     var g = UIHelper.CreateGroup(group.Transform, "Password", Axis.Horizontal);
@@ -71,7 +84,78 @@ namespace SpinArchipelago
 
         private static void ConnectToServer()
         {
-            NotificationSystemGUI.AddMessage("Not implemented yet");
+            if (string.IsNullOrWhiteSpace(_playerName))
+            {
+                NotificationSystemGUI.AddMessage("Player Name is empty");
+                return;
+            }
+            var addressParts = _serverAddress?.Split(':') ?? Array.Empty<string>();
+            if (addressParts.Length != 2)
+            {
+                NotificationSystemGUI.AddMessage("Invalid address");
+                return;
+            }
+
+            string address = addressParts[0];
+            if (!ushort.TryParse(addressParts[1], out ushort port))
+            {
+                NotificationSystemGUI.AddMessage("Invalid port");
+                return;
+            }
+            _session = ArchipelagoSessionFactory.CreateSession(address, port);
+            NotificationSystemGUI.AddMessage("Connecting to Archipelago server");
+            Login();
+        }
+
+        private static void Login()
+        {
+            LoginResult result;
+
+            try
+            {
+                result = _session.TryConnectAndLogin(
+                    "Spin Rhythm XD",
+                    _playerName,
+                    ItemsHandlingFlags.AllItems,
+                    password: _password
+                );
+            }
+            catch (Exception e)
+            {
+                result = new LoginFailure(e.GetBaseException().Message);
+            }
+
+            if (!result.Successful)
+            {
+                var failure = (LoginFailure)result;
+                NotificationSystemGUI.AddMessage("Failed to connect to server: check console logs for details");
+                string failureMessage = $"Failed to connect to {_serverAddress} as {_playerName}:";
+                foreach (string error in failure.Errors)
+                {
+                    failureMessage += $"\n    {error}";
+                }
+                foreach (var error in failure.ErrorCodes)
+                {
+                    failureMessage += $"\n    {error}";
+                }
+                Log.Error(failureMessage);
+                return;
+            }
+
+            //var success = (LoginSuccessful)result;
+            _session.Items.ItemReceived += ReceivedItemHandler;
+            _session.SetClientState(ArchipelagoClientState.ClientConnected);
+            NotificationSystemGUI.AddMessage("Connected!");
+            Log.Info("Connected!");
+        }
+
+        private static void ReceivedItemHandler(ReceivedItemsHelper helper)
+        {
+            while (helper.Any())
+            {
+                var itemInfo = helper.DequeueItem();
+                Log.Info($"CALLBACK: Obtained Item {itemInfo.ItemId} {itemInfo.ItemName} from {itemInfo.Player.Name} in {itemInfo.Player.Game}");
+            }
         }
     }
 }
